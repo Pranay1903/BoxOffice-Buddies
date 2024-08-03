bc45a576082da107735f58c68ef533f05f349754
Pranay Patel <pranaypatel1933@gmail.com>
2024-08-03 23:56:44 +0530

    Update user-side pages: seat.php and movie_details.php

--- user/movie_details.php
+++ user/movie_details.php
@@ -17,7 +17,7 @@ $row = mysqli_fetch_array($result);
   <meta name="keywords" content="Movie, Details, Booking">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?php echo $row['moviename']; ?> Movie Details</title>
+  <title><?php echo htmlspecialchars($row['moviename']); ?> Movie Details</title>
   <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <style>
@@ -34,19 +34,34 @@ $row = mysqli_fetch_array($result);
       .card-img-top {
           width: 100%;
           height: 28vw;
-          object-fit: cover;
+          object-fit: contain; /* Changed from cover to contain */
       }
       .btn {
           margin-top: auto;
       }
       .container-custom {
-          padding-left: 25px;
-          padding-right: 25px;
+          padding-left: 15px;
+          padding-right: 15px;
       }
       .card-body {
           display: flex;
           flex-direction: column;
           justify-content: space-between;
+      }
+      .resize-detail {
+          width: 100%;
+          height: auto;
+          max-height: 400px; /* Adjust the max-height as needed */
+          object-fit: contain; /* Changed from cover to contain */
+      }
+      .card-body dl {
+          margin-bottom: 0;
+      }
+      .card-body dl dt, .card-body dl dd {
+          margin-bottom: 5px;
+      }
+      .table th, .table td {
+          vertical-align: middle;
       }
   </style>
 </head>
@@ -68,7 +83,9 @@ $row = mysqli_fetch_array($result);
           $id = $row['movie_id'];
           ?>
           <div class="row feature design">
-            <div class="col-lg-4"> <img src="image/<?php echo $row['photo']; ?>" class="resize-detail" alt="" width="100%"> </div>
+            <div class="col-lg-4"> 
+              <img src="image/<?php echo htmlspecialchars($row['photo']); ?>" class="resize-detail" alt="Movie Poster"> 
+            </div>
             <div class="col-lg-8">
               <div class="card">
                 <div class="card-header">
@@ -78,15 +95,15 @@ $row = mysqli_fetch_array($result);
                 <div class="card-body">
                   <dl class="row">
                     <dt class="col-lg-4">Movie Name</dt>
-                    <dd class="col-lg-8"><?php echo $row['moviename']; ?></dd>
+                    <dd class="col-lg-8"><?php echo htmlspecialchars($row['moviename']); ?></dd>
                     <dt class="col-lg-4">Release Date</dt>
-                    <dd class="col-lg-8"><?php echo $row['realsedate']; ?></dd>
+                    <dd class="col-lg-8"><?php echo htmlspecialchars($row['realsedate']); ?></dd>
                     <dt class="col-lg-4">Director Name</dt>
-                    <dd class="col-lg-8"><?php echo $row['director']; ?></dd>
+                    <dd class="col-lg-8"><?php echo htmlspecialchars($row['director']); ?></dd>
                     <dt class="col-lg-4">Category</dt>
-                    <dd class="col-lg-8"><?php echo $row['category']; ?></dd>
+                    <dd class="col-lg-8"><?php echo htmlspecialchars($row['category']); ?></dd>
                     <dt class="col-lg-4">Description</dt>
-                    <dd class="col-lg-8"><?php echo $row['description']; ?></dd>
+                    <dd class="col-lg-8"><?php echo htmlspecialchars($row['description']); ?></dd>
                   </dl>
                 </div>
                 <!-- /.card-body -->
@@ -97,9 +114,9 @@ $row = mysqli_fetch_array($result);
 
           <?php
           $s = mysqli_query($conn, "select DISTINCT screen_id from show_details where movie_id='" . $row['movie_id'] . "'");
-          if (mysqli_num_rows($s)) {
+          if (mysqli_num_rows($s) > 0) {
             ?>
-            <div class="connatiner pt-4">
+            <div class="container pt-4">
               <h3 style="color:#e0e0e0; background-color: #444;" class="text-center"><b>Available Shows</b></h3>
               <table class="table table-hover table-bordered text-center">
                 <thead>
@@ -115,7 +132,7 @@ $row = mysqli_fetch_array($result);
                   ?>
                   <tbody>
                     <tr>
-                      <td><?php echo $screen['screen_name'] ?></td>
+                      <td><?php echo htmlspecialchars($screen['screen_name']); ?></td>
                       <td>
                         <?php
                         $tr = mysqli_query($conn, "select * from show_details where movie_id='" . $row['movie_id'] . "' and screen_id='" . $shw['screen_id'] . "'");
@@ -141,7 +158,7 @@ $row = mysqli_fetch_array($result);
               <?php
           } else {
             ?>
-              <h3 style="color:#444; font-size:23px;" class="text-center">Currently there are no any shows available!</h3>
+              <h3 style="color:#444; font-size:23px;" class="text-center">Currently there are no shows available!</h3>
               <p class="text-center">Please check back later!</p>
               <?php
           }
@@ -158,5 +175,6 @@ $row = mysqli_fetch_array($result);
   include("footer.php");
   ?>
 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
--- user/seat.php
+++ user/seat.php
@@ -2,308 +2,231 @@
 session_start();
 include_once 'Database.php';
 if (!isset($_SESSION['uname'])) {
-  header('location:login.php');
+    header('location:login.php');
+    exit;
 }
 if (isset($_GET['show_id'])) {
-  $_SESSION['show_id'] = $_GET['show_id'];
+    $_SESSION['show_id'] = $_GET['show_id'];
 }
 if (isset($_GET['movie_id'])) {
-  $_SESSION['movie_id'] = $_GET['movie_id'];
+    $_SESSION['movie_id'] = $_GET['movie_id'];
 }
 if (isset($_GET['screen_id'])) {
-  $_SESSION['screen_id'] = $_GET['screen_id'];
+    $_SESSION['screen_id'] = $_GET['screen_id'];
 }
-?>
 
-<?php
 if (isset($_SESSION['uname'])) {
-  include("header.php");
-  ?>
-  <style>
-    .container {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-    }
-
-    button {
-      margin-top: 20px;
-      text-align: center;
-    }
-
-    h1,
-    h2,
-    label {
-      text-align: center;
-    }
-
-    label {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-
-    label span {
-      margin-left: 10px;
-    }
-
-    .ticketing-row {
-      display: flex;
-      margin-bottom: 10px;
-    }
-
-    .ticketing-col {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex: 1;
-      height: 30px;
-      background-color: #fff;
-      border: 1px solid #ccc;
-      cursor: pointer;
-    }
-
-    .ticketing-col.reserved {
-      background-color: #ddd;
-      color: #777;
-      cursor: default;
-    }
-
-    .ticketing-col.screen {
-      background-color: #333;
-      color: #fff;
-      font-weight: bold;
-      cursor: default;
-    }
-
-    button.two-button-one,
-    button.two-button-two {
-      margin: 0 auto;
-    }
-
-    button {
-      margin-top: 20px;
-      padding: 10px;
-    }
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      margin-bottom: 20px;
-    }
-
-    td {
-      border: 1px solid black;
-      padding: 10px;
-    }
-
-    td:first-child {
-      width: 50%;
-    }
-
-    .contr {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      padding: 10px;
-      background-color: #f2f2f2;
-      border-radius: 5px;
-    }
-
-    .movie-details {
-      display: flex;
-      flex-direction: column;
-    }
-
-    .movie-details h2 {
-      margin: 0;
-      font-size: 24px;
-    }
-
-    .movie-details p {
-      margin: 0;
-      font-size: 16px;
-    }
-
-    .ticket-details {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-    }
-
-    .ticket-details h2 {
-      margin: 0;
-      font-size: 24px;
-    }
-
-    .ticket-details p {
-      margin: 0;
-      font-size: 16px;
-    }
-  </style>
-  <div class="container">
-    <?php
-    $movie_id = $_GET['movie_id'];
-    $show_id = $_GET['show_id'];
-    $query = "SELECT * FROM movie WHERE movie_id = '$movie_id'";
-
-    $result = mysqli_query($conn, $query);
-    $movie = mysqli_fetch_assoc($result);
-    // $show = mysqli_fetch_assoc(mysqli_query($conn, "SELECT show_time FROM show_details WHERE show_id = '$show_id'"));
-    ?>
-    <div class="movie-details">
-      <h2>Movie Name: <span>
-          <?php echo $movie['moviename']; ?>
-        </span></h2>
-      <img src="<?php echo $movie['photo']; ?>" alt="Movie Poster">
-      <!-- <p>Show Time: <?php echo $show['show_time']; ?></p> -->
-    </div>
-    <div class="ticket-details">
-      <!-- HTML code for selected seat display -->
-      <p>Selected Seat: <span id="selectedSeat"></span></p>
-      <p>Total Price: $<span id="totalPrice">0</span></p>
-
-
-
-    </div>
-  </div>
-
-  <table>
-
-    <div class="col-lg-12">
-      <section class="clearfix">
-        <form method="post" action="buyticket.php" onsubmit="return check();">
-          <h2>Select Your Seat's</h2>
-          <?php
-          // Your PHP code to fetch screen information and seat availability here
-          $query = "SELECT * FROM screen WHERE screen_id =  '" . $_SESSION['screen_id'] . "' ";
-          $record = mysqli_query($conn, $query) or die("Query Error!" . mysqli_error($conn));
-          $houseInfo = mysqli_fetch_array($record);
-          mysqli_free_result($record);
-
-          $query = "SELECT * FROM ticket WHERE show_id = '" . $_SESSION['show_id'] . "'";
-          $record = mysqli_query($conn, $query) or die("Query Error!" . mysqli_error($conn));
-          $seatsOccupied;
-          $numberOfSeatsOccupied = 0;
-          while ($row = mysqli_fetch_array($record)) {
-            $seatsOccupied[$numberOfSeatsOccupied][0] = $row['row'];
-            $seatsOccupied[$numberOfSeatsOccupied][1] = $row['col'];
-            $numberOfSeatsOccupied++;
-          }
-          mysqli_free_result($record);
-          ?>
-          <?php
-          while ($houseInfo['row']) {
-            $rowName = chr(65 + $houseInfo['row'] - 1);
-            ?>
-            
-            <div class="ticketing-row">
-              <?php
-              for ($i = 1; $i <= $houseInfo['col']; $i++) {
-                $isReserved = 0;
-
-                for ($it = 0; $it < $numberOfSeatsOccupied; $it++) {
-                  if ($seatsOccupied[$it][0] == $houseInfo['row'] && $seatsOccupied[$it][1] == $i)
-                    $isReserved = 1;
-                }
-
-                if ($isReserved) {
-                  echo "<div class='ticketing-col reserved'>";
-                  print "Sold " . $rowName . $i;
-                } else {
-                  echo "<div class='ticketing-col'>";
-                  print "<input type='checkbox' class='checkbox' name='seat[]' value='" . $houseInfo['row'] . "|" . $i . "'>";
-                  print $rowName . $i;
-                }
-                echo "</div>";
-              }
-              ?>
-            </div>
-
-            <?php
-            $houseInfo['row']--;
-          }
-          ?>
-          <div class="ticketing-row">
-            <div class="ticketing-col screen">
-              Screen
-            </div>
-          </div>
-          <div style="margin-top: 20px;">
-            <button type="submit" name="submit" id="submit" class="two-button-one" style="display: inline-block;">Select
-              Seat(s)</button>
-            <a href="index.php"><button type="button" name="cancel" class="two-button-two"
-                style="display: inline-block;">Cancel</button></a>
-            <input type="hidden" name="show_id" value="<?php echo $_SESSION['show_id'] ?>">
-            <input type="hidden" name="movie_id" value="<?php echo $_SESSION['movie_id'] ?>">
-
-          </div>
-        </form>
-      </section>
-    </div>
-  </table>
-  </div>
-
-  <?php
-  include("footer.php");
-  ?>
-
-  <?php
-}
-mysqli_close($conn);
+    include("header.php");
 ?>
-
-<script type="text/javascript">
-  function check() {
-    var flag = -1;
-    var listOfCheckboxes = document.getElementsByClassName('checkbox');
-    for (var i = 0; i < listOfCheckboxes.length; i++) {
-      if (listOfCheckboxes[i].checked)
-        flag = 1;
-    }
-    if (flag == -1) {
-      alert("Please choose at least one seat.");
-      return false;
-    }
-  }
-  var seatPrice = <?php echo $houseInfo['price']; ?>; // replace with the actual price of a seat
-  var selectedSeats = [];
-
-  // function to update the selected seats and total price
-  function updateSelectedSeats() {
-  var numSelectedSeats = 0;
-  var selectedSeatText = "";
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      numSelectedSeats++;
-      selectedSeatText += checkboxes[i].value + ", ";
-    }
-  }
-  if (selectedSeatText !== "") {
-    selectedSeatText = selectedSeatText.slice(0, -2); // remove last comma and space
-  }
-  var totalPrice = numSelectedSeats * seatPrice;
-  document.getElementById("selectedSeat").innerHTML = numSelectedSeats;
-  document.getElementById("totalPrice").innerHTML = totalPrice.toFixed(2);
-}
-
-
-  // add event listener to all checkboxes with class "checkbox"
-  var checkboxes = document.getElementsByClassName("checkbox");
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("change", function() {
-      var seat = this.value;
-      if (this.checked) {
-        selectedSeats.push(seat);
-      } else {
-        var index = selectedSeats.indexOf(seat);
-        if (index !== -1) {
-          selectedSeats.splice(index, 1);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie Seat Selection</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <style>
+       
+       html, body {
+        height: 100%;
+        font-family: 'Nunito Sans', sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
         }
+        .heading {
+          padding-top: 20px;
       }
-      updateSelectedSeats();
-    });
-  }
-</script>
+      .container-custom {
+          padding-left: 18px;
+          padding-right: 18px;
+      }
+        .container {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-start;
+            max-width: 1200px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .movie-details {
+            flex: 1;
+            text-align: center;
+            margin-right: 20px;
+        }
+        .movie-details img {
+            width: 300px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .seat-selection {
+            flex: 2;
+            text-align: center;
+        }
+        .seats {
+            display: grid;
+            grid-template-columns: repeat(7, 1fr);
+            gap: 5px;
+            justify-items: center;
+            margin: 20px auto;
+        }
+        .seat {
+            width: 40px;
+            height: 40px;
+            background-color: #fff;
+            border: 2px solid #ccc;
+            border-radius: 4px;
+            text-align: center;
+            line-height: 40px;
+            cursor: pointer;
+            transition: background-color 0.3s, transform 0.3s;
+        }
+        .seat:hover:not(.occupied) {
+            background-color: #e0e0e0;
+            transform: scale(1.1);
+        }
+        .seat.selected {
+            background-color: #6feaf6;
+        }
+        .seat.occupied {
+            background-color: #ff7272;
+            cursor: not-allowed;
+        }
+        .screen {
+            margin: 20px auto;
+            background-color: #444;
+            height: 50px;
+            width: 80%;
+            text-align: center;
+            line-height: 50px;
+            color: white;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+        }
+        .summary {
+            text-align: left;
+            margin-top: 20px;
+        }
+        button {
+            background-color: #007bff;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            margin: 10px;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+        button#cancel-btn {
+            background-color: #6c757d;
+        }
+        button#cancel-btn:hover {
+            background-color: #5a6268;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="movie-details">
+            <?php
+            $movie_id = $_SESSION['movie_id'];
+            $show_id = $_SESSION['show_id'];
+            $query = "SELECT * FROM movie WHERE movie_id = '$movie_id'";
+            $result = mysqli_query($conn, $query);
+            $movie = mysqli_fetch_assoc($result);
+            ?>
+            <h2>Movie Name: <?php echo htmlspecialchars($movie['moviename']); ?></h2>
+            <img src="image/<?php echo htmlspecialchars($movie['photo']); ?>" alt="Movie Poster">
+        </div>
+        <div class="seat-selection">
+            <h3>Select Your Seats</h3>
+            <div class="seats">
+                <?php
+                $query = "SELECT * FROM screen WHERE screen_id = '" . $_SESSION['screen_id'] . "'";
+                $record = mysqli_query($conn, $query) or die("Query Error!" . mysqli_error($conn));
+                $houseInfo = mysqli_fetch_array($record);
+                mysqli_free_result($record);
 
+                $query = "SELECT * FROM ticket WHERE show_id = '" . $_SESSION['show_id'] . "'";
+                $record = mysqli_query($conn, $query) or die("Query Error!" . mysqli_error($conn));
+                $seatsOccupied = [];
+                while ($row = mysqli_fetch_array($record)) {
+                    $seatsOccupied[] = [$row['row'], $row['col']];
+                }
+                mysqli_free_result($record);
+
+                $rows = $houseInfo['row'];
+                $cols = $houseInfo['col'];
+                $seatNumbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+                for ($i = 1; $i <= $rows; $i++) {
+                    for ($j = 1; $j <= $cols; $j++) {
+                        $isReserved = in_array([$i, $j], $seatsOccupied);
+                        $seatClass = $isReserved ? 'seat occupied' : 'seat';
+                        echo "<div class='$seatClass' data-row='$i' data-col='$j'>" . $seatNumbers[$i - 1] . $j . "</div>";
+                    }
+                }
+                ?>
+            </div>
+            <div class="screen">Screen</div>
+            <div class="summary">
+                <p>Selected Seat: <span id="selected-seat">None</span></p>
+                <p>Total Price: $<span id="total-price">0</span></p>
+            </div>
+            <form method="post" action="buyticket.php" onsubmit="return check();">
+                <button type="submit" id="select-seat-btn">Select Seat(s)</button>
+                <button type="button" id="cancel-btn">Cancel</button>
+                <input type="hidden" name="show_id" value="<?php echo htmlspecialchars($_SESSION['show_id']); ?>">
+                <input type="hidden" name="movie_id" value="<?php echo htmlspecialchars($_SESSION['movie_id']); ?>">
+                <input type="hidden" name="screen_id" value="<?php echo htmlspecialchars($_SESSION['screen_id']); ?>">
+            </form>
+        </div>
+    </div>
+
+    <script>
+        const seats = document.querySelectorAll('.seat:not(.occupied)');
+        const selectedSeatEl = document.getElementById('selected-seat');
+        const totalPriceEl = document.getElementById('total-price');
+        const selectSeatBtn = document.getElementById('select-seat-btn');
+        const cancelBtn = document.getElementById('cancel-btn');
+
+        let selectedSeats = [];
+
+        seats.forEach(seat => {
+            seat.addEventListener('click', () => {
+                if (!seat.classList.contains('occupied')) {
+                    seat.classList.toggle('selected');
+                    updateSelectedSeats();
+                }
+            });
+        });
+
+        function updateSelectedSeats() {
+            selectedSeats = [...document.querySelectorAll('.seat.selected')].map(seat => seat.innerText);
+            selectedSeatEl.innerText = selectedSeats.join(', ') || 'None';
+            totalPriceEl.innerText = selectedSeats.length * 10; // Assuming each seat costs $10
+        }
+
+        selectSeatBtn.addEventListener('click', () => {
+            alert(`Selected Seats: ${selectedSeats.join(', ')}\nTotal Price: $${selectedSeats.length * 10}`);
+        });
+
+        cancelBtn.addEventListener('click', () => {
+            selectedSeats = [];
+            document.querySelectorAll('.seat.selected').forEach(seat => seat.classList.remove('selected'));
+            updateSelectedSeats();
+        });
+    </script>
+</body>
+</html>
+<?php
+    include("footer.php");
+}
+?>
